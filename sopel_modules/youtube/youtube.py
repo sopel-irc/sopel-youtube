6e0f061e77cd0bbdc775eb253997a6fd70a335e1
Harri Berglund <Ohcanep@users.noreply.github.com>
2015-10-01 15:59:15 +0300

    Fix configure()
    Don't validate [youtube] section in configure() as it will fail
    immediately unless api_key has been added to the config already.

--- sopel_modules/youtube/youtube.py
+++ sopel_modules/youtube/youtube.py
@@ -18,8 +18,8 @@ class YoutubeSection(StaticSection):
 
 
 def configure(config):
-    config.define_section('youtube', YoutubeSection)
-    config.bugzilla.configure_setting(
+    config.define_section('youtube', YoutubeSection, validate=False)
+    config.youtube.configure_setting(
         'api_key',
         'Enter your Google API key.',
     )
